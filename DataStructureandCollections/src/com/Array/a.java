203f8ecf231a1b725a54f6a07b02412c0017b854
mayurishegokar <mayurishegokar152@gmail.com>
2025-10-02 14:52:29 +0530

    aaaaaaaaaa

--- DataStructureandCollections/src/com/Array/a.java
+++ DataStructureandCollections/src/com/Array/a.java
@@ -1,28 +1,34 @@
 package com.Array;
 
+import java.util.Scanner;
+
 public class a {
-
+	
 	public static void main(String[] args) {
-		System.out.println("lkdjesiulhjs");
-		System.out.println("lkdjesiulhjs");
-		System.out.println("lkdjesiulhjs");
 		
-		System.out.println("lkdjesiulhjs");
-		System.out.println("lkdjesiulhjs");
-		System.out.println("lkdjesiulhjs");
-
-		System.out.println("lkdjesiulhjs");
-		System.out.println("lkdjesiulhjs");
-		System.out.println("lkdjesiulhjs");
+		int[] arr= {10,15,20,25,30,35,40,45};
+		int[] newArr=new int[arr.length-1];
+		int delement=25;
 		
+		int j=0;
 		
-		System.out.println("lkdjesiulhjs");
-		System.out.println("lkdjesiulhjs");
-		System.out.println("lkdjesiulhjs");
-		
-		
-
-		
+		for(int i=0;i<arr.length;i++)
+		{
+			
+			if(arr[i]==delement)
+			{
+			 	continue;	
+			}
+			newArr[j]=arr[i];
+			j++;
+			
+			
+		}
+		System.out.println("Delete "+delement+" in the Array");
+		for(int array:newArr)
+		{
+			System.out.print(array+" ");
+		}
 	}
 
 }
