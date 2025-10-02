533979ae2d1d4e00af3a9d4fc8ad64592cc566de
mayurishegokar <mayurishegokar152@gmail.com>
2025-10-02 13:53:57 +0530

    asss

--- DataStructureandCollections/src/com/Array/a.java
+++ DataStructureandCollections/src/com/Array/a.java
@@ -3,8 +3,11 @@ package com.Array;
 public class a {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("lkdjesiulhjs");
+		System.out.println("lkdjesiulhjs");
+		System.out.println("lkdjesiulhjs");
 
+		
 	}
 
 }
