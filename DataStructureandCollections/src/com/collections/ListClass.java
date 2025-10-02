462244ca72a2ba07fce0204f497e5deb7a888585
mayurishegokar <mayurishegokar152@gmail.com>
2025-10-02 15:48:25 +0530

    add element in List

---
+++ DataStructureandCollections/src/com/collections/ListClass.java
@@ -0,0 +1,22 @@
+package com.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListClass {
+
+	public static void main(String[] args) {
+	
+		List list=new ArrayList<>();
+		
+		list.add(12);
+		list.add(100);
+		list.add("JIYAAA");
+		list.add(3,"Mayuri");
+		
+		System.out.println(list);
+		
+
+	}
+
+}
